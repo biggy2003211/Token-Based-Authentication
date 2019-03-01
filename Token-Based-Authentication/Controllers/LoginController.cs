acd9c249e0d3d7a0a4c0e589090ae375a703a1d2
LeonBing <biggy2003211@gmail.com>
2019-03-01 16:24:12 +0800

    建立登入驗證 Controllers

---
+++ Token-Based-Authentication/Controllers/LoginController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Token_Based_Authentication.App_Start;
+
+namespace Token_Based_Authentication.Controllers
+{
+    public class LoginController : ApiController
+    {
+        [HttpGet]
+        public APIResult Get()
+        {
+            return new APIResult()
+            {
+                Success = true,
+                Message = "",
+                Payload = "Access Token"
+            };
+        }
+    }
+}
