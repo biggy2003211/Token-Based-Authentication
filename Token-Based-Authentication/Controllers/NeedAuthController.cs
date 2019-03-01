545fc417efc02d0baf053a7dc1d589b1f5426a03
LeonBing <biggy2003211@gmail.com>
2019-03-01 22:51:11 +0800

    建立檢查是否存在合法的存取權杖

--- Token-Based-Authentication/Controllers/NeedAuthController.cs
+++ Token-Based-Authentication/Controllers/NeedAuthController.cs
@@ -5,18 +5,23 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using Token_Based_Authentication.App_Start;
+using Token_Based_Authentication.Filters;
 
 namespace Token_Based_Authentication.Controllers
 {
+    [JwtAuth]
     public class NeedAuthController : ApiController
     {
         [HttpGet]
+        [AuthorizeAttribute(Roles= "Admin,Manager,Member")]
         public APIResult Get()
         {
+            var fooUser = Request.Properties["user"] as string;
+
             return new APIResult()
             {
                 Success = true,
-                Message = "需要提供存取權杖 Access Token 才能使用的 API",
+                Message = $"授權使用者為 {fooUser}",
                 Payload = new string[] { "有提供存取權杖1", "有提供存取權杖2" }
             };
         }
